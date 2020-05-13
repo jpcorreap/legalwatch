03ea33cec77687668697770cd26f9b1b6197f49b
Juan Pablo Correa Puerta <jp.correap@uniandes.edu.co>
2020-05-12 21:04:41 -0500

    Home listo

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Navbar from "./components/Navbar.js";
 import Pasos from "./components/Pasos.js";
-import Header from "./components/Header.js";
-import quiobo from "./quiobo.png";
 import "./App.css";
 
 function App() {
@@ -27,32 +25,6 @@ function App() {
       )}
       <br />
       <br />
-
-      <Header />
-      <div className="container">
-        <div className="row">
-          <div className="col-md-8">
-          <hr />
-            <p className="letraPeque">
-              {" "}
-              Bienvenido a My legal watch, somos un sitio web con el propósito
-              de orientarte y ayudarte en el proceso para obtener una patente en
-              Colombia. De una manera fácil de usar te brindamos la información
-              que necesitas en un solo lugar y te acompañamos en cada uno de los
-              pasos para ver tu idea patentada.
-            </p>
-            <a
-              className="btn btn-primary btn-lg"
-              href="#"
-              routerLink="/comenzar">
-              Comenzar &raquo;
-            </a>
-          </div>
-          <div className="col-md-4">
-            <img src={quiobo} width="100%" />
-          </div>
-        </div>
-      </div>
 
       {user ? (
         <Pasos autenticado={true} username={user.username} />
--- front/src/components/Pasos.js
+++ front/src/components/Pasos.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Link } from "react-router-dom";
 import Home from "./Home.js";
 import Login from "./Login.js";
 import Register from "./Register.js";
 import ProgressBar from "./ProgressBar.js";
+import Header from "./Header.js";
 import Paso1 from "./Paso1.js";
 import Paso2 from "./Paso2.js";
 import Paso3 from "./Paso3.js";
@@ -15,12 +16,120 @@ import Paso4 from "./Paso4.js";
 import Paso5 from "./Paso5.js";
 import Paso6 from "./Paso6.js";
 import Solicitudes from "./Solicitudes.js";
+import quiobo from "../quiobo.png";
+import banner5 from "../banner5.PNG";
+import banner6 from "../banner6.PNG";
+import banner7 from "../banner7.PNG";
 
 function Pasos(props) {
   return (
     <div>
       <Switch>
         <Route exact path="/">
+          <Header />
+          <div>
+            <br />
+            <br />
+            <div className="container">
+              <div className="row">
+                <div className="col-8">
+                  <p className="letraPeque text-justify">
+                    Bienvenido a Legal Watch, somos un sitio web con el
+                    propósito de orientarte y ayudarte en el proceso para
+                    obtener una patente en Colombia. De una manera fácil de usar
+                    te brindamos la información que necesitas en un solo lugar y
+                    te acompañamos en cada uno de los pasos para ver tu idea
+                    patentada.
+                  </p>
+                </div>
+                <div className="col-4">
+                  <img src={quiobo} width="100%" alt=" " />
+                </div>
+                <div className="col-12">
+                  <Link to={"/antesdecomenzar"}>
+                    <button
+                      className="btn btn-success"
+                      style={{ fontSize: "25px" }}>
+                      Comenzar
+                    </button>
+                  </Link>
+
+                  <div className="row" style={{ marginTop: "100px" }}>
+                    <div className="col-md-4">
+                      <div className="card h-700">
+                        <img
+                          className="card-img-top"
+                          src={banner7}
+                          alt=" "
+                          width="100%"
+                        />
+                        <div className="card-body text-center">
+                          <h5 className="card-title">
+                            Cifras de patentes en Colombia
+                          </h5>
+                          <a
+                            href="#"
+                            routerLink="/acerca"
+                            className="btn btn-success">
+                            <strong>¡Descubre más!</strong>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="col-md-4">
+                      <div className="card h-700">
+                        <img
+                          className="card-img-top"
+                          src={banner6}
+                          alt=" "
+                          width="100%"
+                        />
+                        <div className="card-body text-center">
+                          <h5 className="card-title">
+                            ¿Cómo podemos ayudarte?
+                          </h5>
+                          <a
+                            href="#"
+                            routerLink="/comenzar"
+                            className="btn btn-success">
+                            <strong>¡Descubre más!</strong>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                    <br />
+                    <br />
+                    <div className="col-md-4">
+                      <div className="card h-700">
+                        <img
+                          className="card-img-top"
+                          src={banner5}
+                          alt=" "
+                          width="100%"
+                        />
+                        <div className="card-body text-center">
+                          <h5 className="card-title">Estado de tu patente</h5>
+                          <a
+                            href="#"
+                            routerLink="/cinco"
+                            className="btn btn-success text-center">
+                            <strong>¡Descubre más!</strong>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <br />
+            <br />
+            <br />
+          </div>
+        </Route>
+
+        <Route exact path="/antesdecomenzar">
           <Home />
         </Route>
 
